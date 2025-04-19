40baea85f113dac9fb7262dec4911e58bbeae3d4
AbsoluteZer0527 <zhuluyuan200527@gmail.com>
2025-04-19 04:18:53 -0700

    Working

--- My project/Assets/Scripts/PauseMenuLogic.cs
+++ My project/Assets/Scripts/PauseMenuLogic.cs
@@ -16,6 +16,8 @@ public class PauseMenuLogic : MonoBehaviour
 
     ObjectSoundEffects sawSounds;
 
+    public BGMSceneManager bgmSceneManager;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
